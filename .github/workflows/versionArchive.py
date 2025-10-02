b1913cf97cc721d00a28e5ddd3e1af21f27f57af
WahPlus <162732679+WahPlus@users.noreply.github.com>
2025-10-02 21:25:51 +0300

    Update versionArchive.py

--- .github/workflows/versionArchive.py
+++ .github/workflows/versionArchive.py
@@ -1,3 +1,16 @@
 import json
-agConfigContents = json.load(open('/home/runner/work/ArgentumPackages/ArgentumPackages/ag2.json'))
-print(agConfigContents)
+import os
+
+os.chdir("/home/runner/work/ArgentumPackages/ArgentumPackages/")
+
+agConfig = json.load(open("./ag2.json"))
+
+for package in agConfig:
+  if not os.path.isdir("./" + package + "-versions/"):
+    os.mkdir("./" + package + "-versions/")
+  versionsSaved = []
+  for dirPath, dirNames, fileNames in os.walk("./" + package + "-versions/"):
+    versionsSaved.extend(dirNames)
+    break
+    if agConfig[package]["version"] not in versionsSaved:
+      print("Stinky!")
