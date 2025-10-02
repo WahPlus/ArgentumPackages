c5292011820fe7072e306236b96ba382d8edcc49
WahPlus <162732679+WahPlus@users.noreply.github.com>
2025-10-02 20:50:38 +0300

    Update versionArchive.py

--- .github/workflows/versionArchive.py
+++ .github/workflows/versionArchive.py
@@ -1,3 +1,3 @@
 import json
-agConfigContents = json.load(open('/home/runner/ArgentumPackages/ArgentumPackages/ag2.json'))
+agConfigContents = json.load(open('/home/runner/work/ArgentumPackages/ArgentumPackages/ag2.json'))
 print(agConfigContents)
