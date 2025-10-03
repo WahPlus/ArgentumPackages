f13b13ab10c2848a27f258c5e67aa0420a035dd8
WahPlus <162732679+WahPlus@users.noreply.github.com>
2025-10-03 15:53:06 +0300

    Update versionArchive.py

--- .github/workflows/versionArchive.py
+++ .github/workflows/versionArchive.py
@@ -20,7 +20,7 @@ for package in agConfig:
       json.dump({"package": agConfig[package]}, f, sort_keys=True, indent=4)
     if agConfig[package]["directories"]:
       for i in agConfig[package]["directories"]:
-        os.mkdir(os.path.join(currentVersionDir, agConfig[package]["directories"][i])
+        os.mkdir(os.path.join(currentVersionDir, agConfig[package]["directories"][i]))
     if agConfig[package]["files"]:
       for i in agConfig[package]["files"]:
         shutil.copyfile(os.path.join("./", agConfig[package]["files"][i]), os.path.join(currentVersionDir, agConfig[package]["files"][i]))
